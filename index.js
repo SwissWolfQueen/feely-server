b5a082c3c1c7658c49cc97457f2c47a95b08c361
SwissWolfQueen <katidzoni@gmail.com>
2017-05-11 00:43:57 +0200

    server feely

--- index.js
+++ index.js
@@ -9,10 +9,20 @@ const root = __dirname.replace("server","dist")
 
 const api = require('./modules/api').api;
 
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
 app
 	.use(express.static(root))
 	.use(bodyParser.urlencoded({extended: true}))
 	.use(bodyParser.json())
+	.use(allowCrossDomain);
 
 app
 	.get('/list', api.getItems)
